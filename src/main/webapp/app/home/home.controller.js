372422e978deb5070d8e71b63c42be7d3876c01b
aloha <radomir.aloha@gmail.com>
2016-06-25 23:44:36 +0200

    xml upload

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,9 +5,9 @@
         .module('pinfProApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Upload', '$timeout'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController ($scope, Principal, LoginService, $state,  Upload, $timeout) {
         var vm = this;
 
         vm.account = null;
@@ -29,5 +29,39 @@
         function register () {
             $state.go('register');
         }
+
+
+
+
+        /* file upload - sve samo radi, da nema angulara riko bi*/
+
+        $scope.uploadFiles = function(file, errFiles) {
+                $scope.f = file;
+                $scope.errFile = errFiles && errFiles[0];
+                if (file) {
+                    file.upload = Upload.upload({
+                        url: '/api/upload',
+                        data: {file: file}
+                    });
+
+                    file.upload.then(function (response) {
+                        $timeout(function () {
+                            file.result = response.data;
+                        });
+                    }, function (response) {
+                        if (response.status > 0)
+                            $scope.errorMsg = response.status + ': ' + response.data;
+                    }, function (evt) {
+                        file.progress = Math.min(100, parseInt(100.0 *
+                                                 evt.loaded / evt.total));
+                    });
+                }
+            }
+
+
+
+
+
+
     }
 })();
--- src/main/webapp/app/layouts/navbar/navbar.controller.js
+++ src/main/webapp/app/layouts/navbar/navbar.controller.js
@@ -5,9 +5,9 @@
         .module('pinfProApp')
         .controller('NavbarController', NavbarController);
 
-    NavbarController.$inject = ['$state', 'Auth', 'Principal', 'ENV', 'LoginService'];
+    NavbarController.$inject = ['$state', 'Auth', 'Principal', 'ENV', 'LoginService', '$scope'];
 
-    function NavbarController ($state, Auth, Principal, ENV, LoginService) {
+    function NavbarController ($state, Auth, Principal, ENV, LoginService, $scope) {
         var vm = this;
 
         vm.isNavbarCollapsed = true;
@@ -37,5 +37,89 @@
         function collapseNavbar () {
             vm.isNavbarCollapsed = true;
         }
+
+
+  //-------------------file----------------------//
+
+       /* $scope.uploadFile=function(){
+
+        var formData=new FormData();
+            formData.append("file",file.files[0]);
+            $http.post('/serverApp/rest/newDocument', formData, {
+                transformRequest: function(data, headersGetterFunction) {
+                    return data;
+                },
+                headers: { 'Content-Type': undefined }
+                }).success(function(data, status) {
+                    alert("Success ... " + status);
+                }).error(function(data, status) {
+                    alert("Error ... " + status);
+                    });
+        }
+
+
+
+         $scope.setFiles = function(element) {
+            $scope.$apply(function($scope) {
+              console.log('files:', element.files);
+              // Turn the FileList object into an Array
+                $scope.files = []
+                for (var i = 0; i < element.files.length; i++) {
+                  $scope.files.push(element.files[i])
+                }
+              $scope.progressVisible = false
+              });
+            };
+
+            $scope.uploadFile = function() {
+                var fd = new FormData()
+                for (var i in $scope.files) {
+                    fd.append("uploadedFile", $scope.files[i])
+                }
+                var xhr = new XMLHttpRequest()
+                xhr.upload.addEventListener("progress", uploadProgress, false)
+                xhr.addEventListener("load", uploadComplete, false)
+                xhr.addEventListener("error", uploadFailed, false)
+                xhr.addEventListener("abort", uploadCanceled, false)
+                xhr.open("POST", "/fileupload")
+                scope.progressVisible = true
+                xhr.send(fd)
+            }
+
+
+
+
+            function uploadProgress(evt) {
+                $scope.$apply(function(){
+                    if (evt.lengthComputable) {
+                        $scope.progress = Math.round(evt.loaded * 100 / evt.total)
+                    } else {
+                        $scope.progress = 'unable to compute'
+                    }
+                })
+            }
+
+            function uploadComplete(evt) {
+                *//* This event is raised when the server send back a response *//*
+                alert(evt.target.responseText)
+            }
+
+            function uploadFailed(evt) {
+                alert("There was an error attempting to upload the file.")
+            }
+
+            function uploadCanceled(evt) {
+                $scope.$apply(function(){
+                    scope.progressVisible = false
+                })
+                alert("The upload has been canceled by the user or the browser dropped the connection.")
+            }
+
+*/
+
+
+
+
+
     }
 })();

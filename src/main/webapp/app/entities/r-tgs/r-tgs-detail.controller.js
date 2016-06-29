d2cfe291d4613f188a8debd0ca4c63af4bca58c7
Nebojsa Popovic <soneneso93@gmail.com>
2016-06-29 23:47:37 +0200

    Rtgs downloading, not finished yet

--- src/main/webapp/app/entities/r-tgs/r-tgs-detail.controller.js
+++ src/main/webapp/app/entities/r-tgs/r-tgs-detail.controller.js
@@ -5,16 +5,32 @@
         .module('pinfProApp')
         .controller('RTGSDetailController', RTGSDetailController);
 
-    RTGSDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'entity', 'RTGS', 'AnalitikaIzvoda'];
+    RTGSDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'entity', 'RTGS', 'AnalitikaIzvoda','$http'];
 
-    function RTGSDetailController($scope, $rootScope, $stateParams, entity, RTGS, AnalitikaIzvoda) {
+    function RTGSDetailController($scope, $rootScope, $stateParams, entity, RTGS, AnalitikaIzvoda,$http) {
         var vm = this;
         vm.rTGS = entity;
-        
+
         var unsubscribe = $rootScope.$on('pinfProApp:rTGSUpdate', function(event, result) {
             vm.rTGS = result;
         });
         $scope.$on('$destroy', unsubscribe);
 
+
+
+
+
+    $scope.generateXml = function(id){
+            var resourceUrl = 'api/generatexml/'+id;
+                    $http({
+                            method: 'GET',
+                            url: resourceUrl
+                        }).then(function successCallback(response) {
+//                            $state.go('banka-izvod', {sifraBanke : sifraBanke });
+                            //alert("USPELI SMO");
+                        }, function errorCallback(response) {
+                            alert("Doslo je do greske. Izvod banke nije generisan.");
+                        });
+    }
     }
 })();
